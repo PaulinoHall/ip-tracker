7e7b8e7673fecde97b811e32e47a38490e33d445
PaulinoHall <paolagalvezmedina@gmail.com>
2026-03-19 18:47:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 
 app = Flask(__name__)
 
+visitas = []
+
 def obtener_pais(ip):
     try:
         res = requests.get(f"http://ip-api.com/json/{ip}").json()
@@ -17,14 +19,34 @@ def inicio():
     fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     pais = obtener_pais(ip)
 
-    # Guardar en archivo
-    with open("visitas.txt", "a") as f:
-        f.write(f"{fecha} - {ip} - {pais}\n")
+    visitas.append({
+        "ip": ip,
+        "fecha": fecha,
+        "pais": pais
+    })
 
     return f"""
     <h1>Bienvenido</h1>
     <p>Tu IP es: {ip}</p>
     <p>País: {pais}</p>
+    """
+
+@app.route("/admin")
+def admin():
+    tabla = ""
+    for v in visitas:
+        tabla += f"<tr><td>{v['ip']}</td><td>{v['fecha']}</td><td>{v['pais']}</td></tr>"
+
+    return f"""
+    <h1>Panel de Visitas</h1>
+    <table border="1">
+        <tr>
+            <th>IP</th>
+            <th>Fecha</th>
+            <th>País</th>
+        </tr>
+        {tabla}
+    </table>
     """
 
 import os
