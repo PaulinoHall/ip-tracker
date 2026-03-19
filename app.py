0115d21aee5a9b2a6e614c52a84b83e20dd2bdd8
PaulinoHall <paolagalvezmedina@gmail.com>
2026-03-19 19:04:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,78 @@
 from flask import Flask, request
 from datetime import datetime
 import requests
+import os
 
 app = Flask(__name__)
 
 visitas = []
 
-def obtener_pais(ip):
+def obtener_info_ip(ip):
     try:
         res = requests.get(f"http://ip-api.com/json/{ip}").json()
-        return res.get("country", "Desconocido")
+        return {
+            "pais": res.get("country", "Desconocido"),
+            "ciudad": res.get("city", "Desconocido"),
+            "isp": res.get("isp", "Desconocido")
+        }
     except:
-        return "Desconocido"
+        return {
+            "pais": "Desconocido",
+            "ciudad": "Desconocido",
+            "isp": "Desconocido"
+        }
 
 @app.route("/")
 def inicio():
-   if request.headers.get('X-Forwarded-For'):
-    ip = request.headers.get('X-Forwarded-For').split(',')[0]
-else:
-    ip = request.remote_addr
+    # Obtener IP real
+    if request.headers.get('X-Forwarded-For'):
+        ip = request.headers.get('X-Forwarded-For').split(',')[0]
+    else:
+        ip = request.remote_addr
+
     fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    pais = obtener_pais(ip)
+    info = obtener_info_ip(ip)
 
     visitas.append({
         "ip": ip,
         "fecha": fecha,
-        "pais": pais
+        "pais": info["pais"],
+        "ciudad": info["ciudad"],
+        "isp": info["isp"]
     })
 
     return f"""
     <h1>Bienvenido</h1>
     <p>Tu IP es: {ip}</p>
-    <p>País: {pais}</p>
+    <p>{info['ciudad']}, {info['pais']}</p>
     """
 
 @app.route("/admin")
 def admin():
     tabla = ""
     for v in visitas:
-        tabla += f"<tr><td>{v['ip']}</td><td>{v['fecha']}</td><td>{v['pais']}</td></tr>"
+        tabla += f"""
+        <tr>
+            <td>{v['ip']}</td>
+            <td>{v['fecha']}</td>
+            <td>{v['pais']}</td>
+            <td>{v['ciudad']}</td>
+            <td>{v['isp']}</td>
+        </tr>
+        """
 
     return f"""
-    <h1>Panel de Visitas</h1>
+    <h1>Panel PRO 😎</h1>
     <table border="1">
         <tr>
             <th>IP</th>
             <th>Fecha</th>
             <th>País</th>
+            <th>Ciudad</th>
+            <th>ISP</th>
         </tr>
         {tabla}
     </table>
     """
 
-import os
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
